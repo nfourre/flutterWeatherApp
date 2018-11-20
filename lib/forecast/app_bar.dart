08f05c59e260852d8e5a44ee6382922356f5bba5
Nicolas <nicolas.fourre@ineat-conseil.fr>
2018-11-20 18:11:02 +0100

    initial commit + add Background + add AppBar + add Drawer

---
+++ lib/forecast/app_bar.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class ForecastAppBar extends StatelessWidget {
+
+  final Function onDrawerArrowTap;
+  final String selectedDay;
+
+  ForecastAppBar({
+    this.onDrawerArrowTap,
+    this.selectedDay,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      centerTitle: false,
+      backgroundColor: Colors.transparent,
+      elevation: 0.0,
+      title: new Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            'Jeudi 30 Décembre',
+            style: new TextStyle(
+              color: Colors.white,
+              fontSize: 14.0,
+            ),
+          ),
+          Text(
+            'Sacramento',
+            style: new TextStyle(
+              color: Colors.white,
+              fontSize: 30.0,
+            ),
+          ),
+        ],
+      ),
+      actions: <Widget>[
+        new IconButton(
+          icon: new Icon(
+            Icons.arrow_forward_ios,
+            color: Colors.white,
+            size: 35.0,
+          ),
+          onPressed: onDrawerArrowTap,
+        )
+      ],
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:weather/forecast/app_bar.dart';
+import 'package:weather/forecast/background/background_with_rings.dart';
+import 'package:weather/forecast/week_drawer.dart';
+import 'package:weather/generic_widgets/sliding_drawer.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Weather',
+      debugShowCheckedModeBanner: false,
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
+
+  OpenableController openableController;
+  String selectedDay = 'Monday, August 26';
+
+  @override
+  void initState() {
+    super.initState();
+
+    openableController = new OpenableController(
+      vsync: this,
+      openDuration: const Duration(milliseconds: 250),
+    )
+    ..addListener(() => setState(() {}));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: new Stack(
+        children: <Widget>[
+          BackgroundWithRings(),
+          
+          Positioned(
+            top: 0.0,
+            left: 0.0,
+            right: 0.0,
+            child: new ForecastAppBar(
+              onDrawerArrowTap: openableController.open,
+              selectedDay: selectedDay,
+            ),
+          ),
+          
+          SlidingDrawer(
+            openableController: openableController,
+            drawer: new WeekDrawer(
+              onDaySelected: (String title) {
+                setState(() {
+                  selectedDay = title.replaceAll('\n', ', ');
+                });
+                openableController.close();
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
